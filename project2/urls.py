ad9adf7ac7b6c6349a14ade10d9ab5783084e8a7
ecemkrpyk <ecemkrpyk.98@gmail.com>
2021-03-25 13:30:57 +0300

    Apartment uygulaması ve Category tablosu eklendi

--- project2/urls.py
+++ project2/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),#bir şey yazmadan home ya gitmesini istiyoruz
+
     path('home/', include('home.urls')),
+    path('apartment/', include('apartment.urls')),
     path('admin/', admin.site.urls),
 ]
