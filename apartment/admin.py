ac210ff8b6d028aca2cbc0ef4cdd0c707de0b22b
ecemkrpyk <ecemkrpyk.98@gmail.com>
2021-04-01 21:42:04 +0300

    Adminde yüklenen resimlerin gösterilmesi

--- apartment/admin.py
+++ apartment/admin.py
@@ -8,16 +8,20 @@ class ApartmentImageInline(admin.TabularInline):
     extra = 5   #galeri extra kaç resimden oluştuğu
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title', 'status', 'image_tag']
+    readonly_fields = ('image_tag',)
     list_filter = ['status'] #statuse göre filtreleme
 
 class ApartmentAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'price', 'amount', 'status']
+    list_display = ['title', 'category', 'price', 'amount', 'status', 'image_tag']
+    readonly_fields = ('image_tag',)
     list_filter = ['status', 'category']
     inlines = [ApartmentImageInline]
 
+
 class ImagesAdmin(admin.ModelAdmin):
-    list_display = ['title', 'apartment', 'image']
+    list_display = ['title', 'apartment', 'image_tag']
+    readonly_fields = ('image_tag',)
 
 admin.site.register(Category, CategoryAdmin)  #categorynin adminde gözükmesini sağlar
 admin.site.register(Apartment, ApartmentAdmin)
