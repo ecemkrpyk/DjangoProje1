4315aa782ede03ceff05420cd7544192d10f896c
ecemkrpyk <ecemkrpyk.98@gmail.com>
2021-05-01 14:55:12 +0300

    user panel

--- project2/urls.py
+++ project2/urls.py
@@ -22,14 +22,15 @@ from home import views
 
 urlpatterns = [
     path('', include('home.urls')),  #bir şey yazmadan home ya gitmesini istiyoruz
+    path('home/', include('home.urls')),
+    path('apartment/', include('apartment.urls')),
+    path('user/', include('user.urls')),
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 
     path('hakkimizda/', views.hakkimizda, name='hakkimizda'),
     path('referanslar/', views.referanslar, name='referanslar'),
     path('iletisim/', views.iletisim, name='iletisim'),
-    path('home/', include('home.urls')),
-    path('apartment/', include('apartment.urls')),
-    path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('category/<int:id>/<slug:slug>', views.category_apartments, name='category_apartments'),
     path('apartment/<int:id>/<slug:slug>', views.apartment_detail, name='apartment_detail'),
     path('search/', views.apartment_search, name='apartment_search'),
@@ -37,6 +38,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout_view'),
     path('login/', views.login_view, name='login_view'),
     path('signup/', views.signup_view, name='signup_view'),
+
 
 
 ]
