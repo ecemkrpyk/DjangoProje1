ad6cee3f02034fa71163587b462404169fb63c4d
ecemkrpyk <ecemkrpyk.98@gmail.com>
2021-05-01 11:55:24 +0300

    üye profili-ek bilgiler

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from home.models import Setting, ContactFormMessage
+from home.models import Setting, ContactFormMessage, UserProfile
 
 admin.site.register(Setting)
 
@@ -11,3 +11,25 @@ class ContactFormMessageAdmin(admin.ModelAdmin):
     list_filter=['status']
 
 admin.site.register(ContactFormMessage,ContactFormMessageAdmin)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_name','phone','address','city', 'country', 'image_tag']
+
+
+admin.site.register(UserProfile,UserProfileAdmin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- home/models.py
+++ home/models.py
@@ -1,8 +1,12 @@
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.db import models
 from django.forms import ModelForm, TextInput, Textarea
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Setting(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -66,6 +70,46 @@ class ContactForm(ModelForm): #otomatik form oluşturma için
             'email': TextInput(attrs={'class': 'input', 'placeholder': 'Email Address'}),
             'message': Textarea(attrs={'class': 'input', 'placeholder': 'Your Message', 'rows': '5'}),
         }
+
+class UserProfile(models.Model): #userın ekstra bilgilere ihtiyacımız olabilir
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.CharField(max_length=150, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/users/')
+
+    def __str__(self):
+        return self.user.username
+
+    def user_name(self):
+        return  self.user.first_name  + ' ' + self.user.last_name + '   [' +self.user.username + ']'
+
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['phone', 'address', 'city', 'country', 'image']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
