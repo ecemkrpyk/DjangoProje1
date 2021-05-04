e53894b2fba85f2c03682336be4f3bc2484b09a0
ecemkrpyk <ecemkrpyk.98@gmail.com>
2021-05-04 18:11:25 +0300

    yorumları listeleme silme

--- user/urls.py
+++ user/urls.py
@@ -7,4 +7,21 @@ urlpatterns = [
     path('', views.index, name='index'), #home da herhangi bir şey girilmezse indexi kullan demek
     path('update/', views.user_update, name='user_update'),
     path('password/', views.change_password, name='change_password'),
+    path('comments/', views.comments, name='comments'),
+    path('deletecomment/<int:id>', views.deletecomment, name='deletecomment'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
-from apartment.models import Category
+from apartment.models import Category, Comment
 from home.models import UserProfile
 
 from user.forms import UserUpdateForm, ProfileUpdateForm
@@ -59,6 +60,25 @@ def change_password(request):
         return render(request, 'change_password.html', {
             'form': form, 'category': category
     })
+
+@login_required(login_url='/login')
+def comments(request):
+    category=Category.objects.all()
+    current_user=request.user
+    comments=Comment.objects.filter(user_id=current_user.id)
+
+    context={'category': category,
+             'comments': comments,
+             }
+    return render(request, 'user_comments.html', context)
+
+@login_required(login_url='/login')
+def deletecomment(request,id):
+    current_user = request.user
+    Comment.objects.filter(id=id,user_id=current_user.id).delete()
+    messages.success(request, 'Yorum silindi')
+    return HttpResponseRedirect('/user/comments')
+
 
 
 
